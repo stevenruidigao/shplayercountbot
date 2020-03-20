06e76c74b442a04fede0811315ceca9a81f8b6fb
stevenruidigao <stevenruidigao@gmail.com>
2020-03-20 15:22:26 -0400

    Add ability to check sign ups

--- app.js
+++ app.js
@@ -9,26 +9,47 @@ client.on('ready', () => {
 });
 
 client.on('message', async msg => {
-	if (msg.content === 'player.count' && !msg.author.bot) {
-		reply(msg);
+	if (msg.author.bot) return;
+	if (msg.content === 'player.count') {
+		replyPlayerCount(msg);
+	} else if (msg.content === 'sign.ups') {
+		replySignupsEnabled(msg);
 	}
 });
 
 client.login(config.token);
 
 var count = getOnlineUsers();
+var signupStatus = 'enabled';
+var signupInProgress = false;
 
-async function reply(msg) {
+async function replyPlayerCount(msg) {
 	count = await getOnlineUsers();
-	msg.reply('There are currently ' + count + ' players online.');
+	msg.reply('there are currently ' + count + ' players online.');
+}
+
+async function replySignupsEnabled(msg) {
+	if (signupInProgress) {
+		msg.reply('signups are currently ' + signupStatus);
+		return;
+	}
+	
+	var enabled = await signupsEnabled();
+	signupStatus = (enabled ? 'enabled' : 'disabled');
+	msg.reply('signups are currently ' + signupStatus);
+	
+	if (!enabled) {
+		signupInProgress = true;
+		signup();
+	}
 }
 
 async function updateStatus() {
 	count = await getOnlineUsers();
-	client.user.setPresence({activity: {name: 'There are currently ' + count + ' players online.'}, status: 'online'});
+	client.user.setPresence({activity: {name: count + ' players online.'}, status: 'online'});
 	setInterval(async () => {
 		count = await getOnlineUsers();
-		client.user.setPresence({activity: {name: 'There are currently ' + count + ' players online.'}, status: 'online'});
+		client.user.setPresence({activity: {name: count + ' players online.'}, status: 'online'});
 	}, 60000);
 }
 
@@ -62,7 +83,7 @@ function makeJSONRequest(options) {
 
 async function getOnlineUsers() {
 	var options = {
-		host: 'www.secrethitler.io',
+		host: 'secrethitler.io',
 		path: '/online-playercount',
 		headers: {'User-Agent': 'request'}
 	};
@@ -76,3 +97,149 @@ async function getOnlineUsers() {
 	}
 }
 
+async function makePOSTRequest(options, encodedData) {
+	return new Promise((resolve, reject) => {
+		var json = '';
+		var data = {};
+		
+		var req = https.request(options, res => {
+			res.on('data', chunk => {
+				json += chunk;
+			});
+			res.on('end', () => {
+				if (res.statusCode === 200) {
+					try {
+						data = JSON.parse(json);
+						resolve({
+							error: 0,
+							statusCode: res.statusCode,
+							data: data
+						});
+					} catch (e) {
+						reject({
+							error: e,
+							statusCode: res.statusCode,
+							data: data
+						});
+					}
+				} else {
+					try {
+						data = JSON.parse(json);
+						reject({
+							error: 0,
+							statusCode: res.statusCode,
+							data: data
+						});
+					} catch (e) {
+						reject({
+							error: e,
+							statusCode: res.statusCode,
+							data: data
+						});
+					}
+				}
+			});
+		}).on('error', function (err) {
+			reject({
+				error: err,
+				statusCode: -1,
+				data: ''
+			});
+		});
+		req.write(encodedData);
+		req.end();
+	});
+}
+
+async function signupsEnabled() {
+	if (signupInProgress) return false;
+	signupInProgress = true;
+	
+	var data = {
+		username: 'acsutest',
+		password: config.password,
+		password2: config.password,
+		email: 'acsutest@gmail.com',
+		isPrivate: false,
+		bypassKey: ''
+	}
+	//"username=acsutest&password=redacted&password2=redacted&email=acsutest@gmail.com&isPrivate=false&bypassKey=" https://secrethitler.io/account/signup
+
+	var encodedData = 'username=acsutest&password=' + config.password + '&password2=' + config.password + '&email=acsutest@gmail.com&isPrivate=false&bypassKey='; // JSON.stringify(data);
+	
+	var deleteAccountOptions = {
+		host: 'secrethitler.io',
+		path: '/account/delete-account',
+		method: 'POST',
+        headers: {
+			'Content-Length': Buffer.byteLength(encodedData),
+			'Content-Type': 'application/x-www-form-urlencoded' // 'application/json',
+        }
+	}
+	
+	var signupOptions = {
+		host: 'secrethitler.io',
+		path: '/account/signup',
+		method: 'POST',
+        headers: {
+			'Content-Length': Buffer.byteLength(encodedData),
+			'Content-Type': 'application/x-www-form-urlencoded' // 'application/json',
+        }
+	}
+	
+	try {
+		try {
+			await makePOSTRequest(deleteAccountOptions, encodedData);
+		} catch (e) {
+			signupInProgress = true;
+		}
+		try {
+			let response = await makePOSTRequest(signupOptions, encodedData);
+		} catch (e) {
+			signupStatus = 'disabled';
+			signupInProgress = true;
+		}
+		let data = (await response).data;
+		return !signupInProgress;
+	} catch(e) {
+		signupStatus = 'disabled';
+		signupInProgress = true;
+		return false;
+	}
+}
+
+async function signup() {
+	if (signupInProgress) return;
+	signupInProgress = true;
+	
+	console.log('signupInProgress: ' + signupInProgress);
+	
+	var encodedData = 'username=acsutest&password=' + config.password + '&password2=' + config.password + '&email=acsutest@gmail.com&isPrivate=false&bypassKey=';
+	
+	var signupOptions = {
+		host: 'secrethitler.io',
+		path: '/account/signup',
+		method: 'POST',
+        headers: {
+			'Content-Length': Buffer.byteLength(encodedData),
+			'Content-Type': 'application/x-www-form-urlencoded' // 'application/json',
+        }
+	}
+	
+	try {
+		try {
+			let response = await makePOSTRequest(signupOptions, encodedData);
+		} catch (e) {
+			signupInProgress = true;
+		}
+		let data = (await response).data;
+		
+		signupStatus = (data.message !== 'Creating new accounts is temporarily disabled most likely due to a spam/bot/griefing attack.  If you need an exception, please contact our moderators on discord.' ? 'enabled' : 'disabled');
+		
+		if (signupInProgress) signup();
+	} catch(e) {
+		signupStatus = 'disabled';
+		signupInProgress = true;
+		setTimeout(signup(), 180000);
+	}
+}
